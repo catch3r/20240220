bff8ea488c8405fed8b6e121ef8f4a7fc2b82954
池宗源 <160603031+catch3r@users.noreply.github.com>
2024-03-22 05:26:44 +0000

    ghjgj

--- lesson10/lesson10_homework.py
+++ lesson10/lesson10_homework.py
@@ -1,9 +1,28 @@
 import random
 import pyinputplus as pyip
+import linecache
 from pprint import pprint
 
 def getNames(num:int) -> list[str]:
+    #1
+    #names:list[str] = []
+    # for line in open('names.txt', encoding='utf-8'):
+    #     line:str = line.rstrip('\n')
+    #     names.append(line)
+
+    # random_names:list[str] = random.choices(names, k=num)
+
+    # return random_names
+    
+    #2
     with open('names.txt', encoding='utf-8') as file:
+        #顧慮記憶體的狀況下採取的寫法
+        # while True:
+        #     line = file.readline()
+        #     if not line:
+        #         break
+        #     pass
+
         rawData:str = file.read()
         names:list[str] = rawData.split('\n')
         random_names:list[str] = random.choices(names, k=num)
@@ -22,10 +41,6 @@ def generateData(names:list[str]) -> list[dict[str:any]]:
             'weight': random.randint(40, 90)
         }
 
-        # newItem['name'] = name
-        # newItem['height'] = random.randint(150, 220)
-        # newItem['weight'] = random.randint(40, 90)
-
         items.append(newItem)
 
     return items
@@ -33,7 +48,7 @@ def generateData(names:list[str]) -> list[dict[str:any]]:
 ############################
 
 def main():
-    num:int = pyip.inputInt('請輸入指定人數(1~30):', min=1, max=50)
+    num:int = pyip.inputInt('請輸入指定人數(1~50):', min=1, max=50)
     names:list[str] = getNames(num)
     items:list[dict[str:any]] = generateData(names)
 
